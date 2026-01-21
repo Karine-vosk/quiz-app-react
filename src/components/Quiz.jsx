740c3d38554344898b38fbbda64b762e42448f41
Karine-vosk <karinevoskanyan.vk@gmail.com>
2026-01-21 15:44:23 +0400

    finished completed view

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback } from 'react';
 import QUESTIONS from '../question.js';
-import quizCompleteLogo from '../assets/quiz-complete.png';
 
 import Question from './Question.jsx';
+import Summary from './Summary.jsx';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -22,12 +22,7 @@ const Quiz = () => {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id='summary'>
-        <img src={quizCompleteLogo} alt='quiz complete logo' />
-        <h2>QuizCompleted!</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
